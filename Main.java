ae5058b9207f9f9f8e7533bac0a87cf296236317
jacklynmendoza61 <58575729+jacklynmendoza61@users.noreply.github.com>
2019-12-13 10:01:03 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+   public static void main (String[] args) {
+	  Taxi x = new Taxi ("PCY461");
+	  x.run(" I will go to Makati");
+	  x.printcurrentlocation();
+	  //x: ,y: 
+	  x.book(0,0);
+	  x.run("I am leaving on a jetPlame");
+	  x.reachedDestination();
+   }   //false
+	  
+	  
+	   
+	   
+   }
 
-}
---
+++ Taxi.java
@@ -0,0 +1,50 @@
+
+public class Taxi {
+   private int x = 0;
+   private int y = 0;
+   String Name = null;
+   int dest_x, dest_y;
+   private String gala;
+   
+   public void run (String gala) {
+	   this.gala = gala;
+	   
+	   for (int )
+   }
+   
+   public void book (int x, int y) {
+	   this.dest_x = x;
+	   this.dest_y = y;
+   }
+   
+   public void reachedDestination() {
+	   this.dest_x = x;
+	   this.dest_y = y;
+   }
+   
+   public void printcurrentlocation() {
+	   System.out.println(x + "" + y);
+   }
+   
+    public Taxi (String Taxi) {
+	   this.Name = Taxi;
+   }
+	   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+ 
+   
+  
+   
+		   
+}
